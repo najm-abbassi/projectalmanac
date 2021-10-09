a87b5a35b6a58c91ccc84fb80d4928f5062a7ff1
najm-abbassi <90458789+najm-abbassi@users.noreply.github.com>
2021-10-09 15:59:49 +0100

    STYLING

---
+++ main.js
@@ -0,0 +1,22 @@
+var hintay = {
+  h: "hintay",
+  one: "advanture kid",
+  two: "kiba girls",
+  three: "angel",
+};
+var shonen = {
+  one: "one piece",
+  two: "hunterXhunter",
+  three: "code geasss",
+};
+var drama = {
+  one: "attack on titan",
+  two: "  full metal alchimist",
+  three: " school days",
+};
+var romance = {
+  one: " your lie in april ",
+  two: "snow white with the red hair",
+  three: " after story ",
+};
+var x = console.log($("#TYPE").val());
