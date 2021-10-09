14e40a56b2c14d7cefc6a0108006af7ae64db01f
najm-abbassi <90458789+najm-abbassi@users.noreply.github.com>
2021-10-09 23:30:26 +0100

    functions

--- main.js
+++ main.js
@@ -5,18 +5,44 @@ var hintay = {
   three: "angel",
 };
 var shonen = {
-  one: "one piece",
-  two: "hunterXhunter",
-  three: "code geasss",
+  s: "shonen",
+  one: " obviously we start with 'one piece'",
+  two: " and in number 2 we have the famous 'hunterXhunter'",
+  three: "and in the third place we find 'code geasss'",
 };
 var drama = {
-  one: "attack on titan",
-  two: "  full metal alchimist",
-  three: " school days",
+  one: "top1 is'attack on titan'",
+  two: " the number2 is 'full metal alchimist'",
+  three: "in the third position we find 'school days'",
 };
 var romance = {
+  r: "romance",
   one: " your lie in april ",
   two: "snow white with the red hair",
   three: " after story ",
 };
-var x = console.log($("#TYPE").val());
+
+function compare1(x, y) {
+  y = $("#TYPE").val();
+  x = Object.values(shonen);
+  for (i = 0; i < x.length; i++) {
+    if (x[i] === y) {
+      console.log(x[1]);
+      console.log(x[2]);
+      console.log(x[3]);
+    }
+  }
+}
+
+function compare2(a, b) {
+  b = $("#TYPE").val();
+  a = Object.values(romance);
+  for (i = 0; i < a.length; i++) {
+    if (a[i] === b) {
+      console.log(a[1]);
+      console.log(a[2]);
+      console.log(a[3]);
+    }
+  }
+}
+
